86143ddc62c893a36a0841f2952c23f0691bdc84
tatsuaki_kin <tatuaki_dear@yahoo.co.jp>
2016-11-13 06:27:06 +0900

    jni

--- Assets/MyProject5/Scripts/native/controller/TopViewController.cs
+++ Assets/MyProject5/Scripts/native/controller/TopViewController.cs
@@ -15,7 +15,22 @@ public class TopViewController : MonoBehaviour {
 
 	private AndroidPlugin m_AndroidPluguin;
 
-	// private int tempValue = 0;
+//	public static string m_FileName = ".testJni2Unity";
+//	public static string m_DeleteFileName = ".testJni2Unity";
+
+	public static string m_FileName = null;
+	public static string m_DeleteFileName = null;
+
+	public const int Release     = 0;
+	public const int Staging     = 1;
+	public const int Develop     = 2;
+
+	public const int Common      = 0;
+	public const int NotCommon   = 1;
+
+	public static string m_Sol_Name     = ".androidFE474DEC";
+	public static string m_Sol_STG      = ".androidFE474DECS";
+	public static string m_Sol_DEV      = ".androidFE474DECD";
 
 	public void Awake()
 	{
@@ -38,6 +53,7 @@ public class TopViewController : MonoBehaviour {
 	{
 		MyLog.I("InitUI");
 		// FB.Init(this.OnInitComplete, this.OnHideUnity);
+
 //		if (GetStartButton() != null) 
 //		{
 //			GetStartButton().onClick.AddListener(() => {
@@ -46,28 +62,31 @@ public class TopViewController : MonoBehaviour {
 //				gameObject.AddComponent<MyHttpRequestManager>();
 //			});
 //		}
-//
+
 //		if (GetBuyButton() != null) 
 //		{
 //			GetBuyButton().onClick.AddListener(() => {
-////				m_IAPHelper.Purchase(null);
-////				if (m_IAPHelper.m_PurchaseInProgress == true) {
-////					return;
-////				}
+//				m_IAPHelper.Purchase(null);
+//				if (m_IAPHelper.m_PurchaseInProgress == true) {
+//					return;
+//				}
 //			});
 //		}
+
 //		if (GetTapjoyButton() != null)
 //		{
 //			GetTapjoyButton().onClick.AddListener(() => {
 //				// m_AdManager.TapjoyEvents();
 //			});
 //		}
+
 //		if (GetFacebookButton() != null) 
 //		{
 //			GetFacebookButton().onClick.AddListener(() => {
 //				// m_AdManager.FBAuth();
 //			});
 //		}
+
 //		if (GetMovieButton() != null) 
 //		{
 //			GetMovieButton().onClick.AddListener(() => {
@@ -85,6 +104,7 @@ public class TopViewController : MonoBehaviour {
 //				// Handheld.PlayFullScreenMovie ("opmv", Color.black, FullScreenMovieControlMode.CancelOnInput);
 //			});
 //		}
+
 //		if (GetSoundButton() != null) 
 //		{
 //			GetSoundButton().onClick.AddListener(() => {
@@ -102,24 +122,99 @@ public class TopViewController : MonoBehaviour {
 //			});
 //		}
 
-		if (GetWriteFileButton() != null) 
+		if (GetCommonWriteFileButton() != null) 
 		{
-			GetWriteFileButton().onClick.AddListener(() => {
-				int writeResult = SgpJni.toNativeWriteCommonFile(".testUnity", "123tesy4910", 0);
-				MyLog.I("GetPushButton toNativeWriteCommonFile = " + writeResult);
+			GetCommonWriteFileButton().onClick.AddListener(() => {
+				int writeResult = SgpJni.CallnativeWriteFile(m_FileName, "CallNativeWriteCommonFile", Common, Staging);
+				MyLog.I("GetNativeWriteButton CallnativeWriteFile writeResult = " + writeResult);
+			});
+		}
+		if (GetNativeReadButton() != null) 
+		{
+			GetNativeReadButton().onClick.AddListener(() => {
+				string commonResurl = SgpJni.CallNativeReadFile(m_FileName, Common, Staging);
+				MyLog.I("GetNativeReadButton CallNativeReadFile commonResurl = " + commonResurl);
+
+				string NotCommonResurl = SgpJni.CallNativeReadFile(m_FileName, NotCommon, Staging);
+				MyLog.I("GetNativeReadButton CallNativeReadFile NotCommonResurl = " + NotCommonResurl);
 				// m_AdManager.FBAuth();
 			});
 		}
 
-		if (GetReadFileButton() != null) 
+		if (GetNativeWriteButton() != null)
 		{
-			GetReadFileButton().onClick.AddListener(() => {
-				string readResult = SgpJni.toNativeReadCommonFile(".androidFE474DEC", 0);
-				MyLog.I("GetReadFileButton toNativeReadCommonFile = " + readResult);
+			GetNativeWriteButton().onClick.AddListener(() => {
+				// int nativeWriteFile(const char* filename, char* text, int common, int svno);
+				int commonResurl = SgpJni.CallnativeWriteFile(m_FileName, "common_CallnativeWriteFile", Common, Staging);
+				MyLog.I("GetNativeWriteButton CallnativeWriteFile commonResurl = " + commonResurl);
+
+				int NotCommonResurl = SgpJni.CallnativeWriteFile(m_FileName, "1234test567890", NotCommon, Staging);
+				MyLog.I("GetNativeWriteButton CallnativeWriteFile NotCommonResurl = " + NotCommonResurl);
 				// m_AdManager.FBAuth();
 			});
 		}
 
+		if (GetSolReadButton() != null) 
+		{
+			GetSolReadButton().onClick.AddListener(() => {
+				string readResult = SgpJni.CallnativeSolReadFile(m_Sol_Name, Common, Release);
+				MyLog.I("nativeSolReadFile readResult = " + readResult);
+
+				string readStg = SgpJni.CallnativeSolReadFile(m_Sol_STG, Common, Staging);
+				MyLog.I("nativeSolReadFile readStg = " + readStg);
+
+				string readDev = SgpJni.CallnativeSolReadFile(m_Sol_DEV, Common, Develop);
+				MyLog.I("nativeSolReadFile readDev = " + readDev);
+
+				string readResult2 = SgpJni.CallnativeSolReadFile(m_Sol_Name, NotCommon, Release);
+				MyLog.I("nativeSolReadFile readResult2 = " + readResult2);
+
+				string readStg2 = SgpJni.CallnativeSolReadFile(m_Sol_STG, NotCommon, Staging);
+				MyLog.I("nativeSolReadFile readStg2 = " + readStg2);
+
+				string readDev2 = SgpJni.CallnativeSolReadFile(m_Sol_DEV, NotCommon, Develop);
+				MyLog.I("nativeSolReadFile readDev2 = " + readDev2);
+			});
+		}
+		if (GetUAButton() != null) 
+		{
+			GetUAButton().onClick.AddListener(() => {
+				string readResult = SgpJni.CallnativeGetUserAgent();
+				MyLog.W("GetUAButton CallnativeGetUserAgent = " + readResult);
+			});
+		}
+
+		if (GetFilePathButton() != null) 
+		{
+			GetFilePathButton().onClick.AddListener(() => {
+				string common = SgpJni.CallgGetFileNameToUnity(m_FileName, Common, Staging);
+				MyLog.W("CallgGetFileNameToUnity common = " + common);
+
+				string notCommon = SgpJni.CallgGetFileNameToUnity(m_FileName, NotCommon, Staging);
+				MyLog.W("CallgGetFileNameToUnity notCommon = " + notCommon);
+			});
+		}
+		if (GetCheckButton() != null) 
+		{
+			GetCheckButton().onClick.AddListener(() => {
+				int check = SgpJni.CallnativeCheckNT();
+				MyLog.W("CallnativeCheckNT = " + check);
+
+				string test = SgpJni.CallnativeTest();
+				MyLog.W("CallnativeTest test = " + test);
+
+			});
+		}
+		if (GetDeleteButton() != null) 
+		{
+			GetDeleteButton().onClick.AddListener(() => {
+				int common = SgpJni.CallNativeDeleteFile(m_DeleteFileName, Common, Staging);
+				MyLog.W("GetDeleteButton GetDeleteButton = " + common);
+
+				int notCommon = SgpJni.CallNativeDeleteFile(m_DeleteFileName, NotCommon, Staging);
+				MyLog.W("GetDeleteButton GetDeleteButton = " + notCommon);
+			});
+		}
 		if (GetNativeButton() != null) 
 		{
 			GetNativeButton().onClick.AddListener(() => {
@@ -155,6 +250,7 @@ public class TopViewController : MonoBehaviour {
 //				// Native
 //			});
 //		}
+
 		if (GetPushButton() != null) 
 		{
 			if (Application.platform == RuntimePlatform.Android) {
@@ -164,24 +260,8 @@ public class TopViewController : MonoBehaviour {
 						m_AndroidPluguin = gameObject.AddComponent<AndroidPlugin>();
 						MyLog.I("add AndroidPlugin");
 					}
-//					string token = m_AndroidPluguin.GetToken("GetAndroidToken");
-//					MyLog.I("GetPushButton token = " + token);
-					// Native
-//					int test = Jnitest.HogeTest();
-//					MyLog.I("GetPushButton HogeTest = " + test);
-//
-//					float tests = Jnitest.floatFunction();
-//					MyLog.I("GetPushButton floatFunction = " + tests);
-
-//					string au = SgpJni.toGetMyName();
-//					MyLog.I("GetPushButton toGetMyName = " + au);
-//
-//					string commons = SgpJni.toGetCommonFileName(".testUnity");
-//					MyLog.I("GetPushButton toGetCommonFileName = " + commons);
-
-					string notCommpn = SgpJni.toGetNotCommonFileName(".testUnity", 0);
-					MyLog.I("GetPushButton toGetNotCommonFileName = " + notCommpn);
-
+					string token = m_AndroidPluguin.GetToken("GetAndroidToken");
+					MyLog.I("GetPushButton token = " + token);
 				});
 			}
 		}
@@ -209,19 +289,16 @@ public class TopViewController : MonoBehaviour {
 		MyLog.D("GetBuyButton");
 		return GameObject.Find("BuyButton").GetComponent<Button>();
 	}
-
 	private Button GetTapjoyButton()
 	{
 		MyLog.D("GetTapjoyButton");
 		return GameObject.Find("TapjoyButton").GetComponent<Button>();
 	}
-
 	private Button GetFacebookButton()
 	{
 		MyLog.D("GetFacebookButton");
 		return GameObject.Find("FacebookButton").GetComponent<Button>();
 	}
-
 	private Button GetStartButton()
 	{
 		MyLog.D("GetStartButton");
@@ -237,15 +314,15 @@ public class TopViewController : MonoBehaviour {
 		MyLog.D("GetSoundButton");
 		return GameObject.Find("SoundButton").GetComponent<Button>();
 	}
-	private Button GetWriteFileButton()
+	private Button GetCommonWriteFileButton()
 	{
-		MyLog.D("GetWriteFileButton");
-		return GameObject.Find("WriteFileButton").GetComponent<Button>();
+		MyLog.D("GetCommonWriteFileButton");
+		return GameObject.Find("CommonWriteFileButton").GetComponent<Button>();
 	}
-	private Button GetReadFileButton()
+	private Button GetSolReadButton()
 	{
-		MyLog.D("GetReadFileButton");
-		return GameObject.Find("ReadFileButton").GetComponent<Button>();
+		MyLog.D("GetSolReadButton");
+		return GameObject.Find("SolReadButton").GetComponent<Button>();
 	}
 	private Button GetNativeButton()
 	{
@@ -262,6 +339,40 @@ public class TopViewController : MonoBehaviour {
 		MyLog.D("GetPushButton");
 		return GameObject.Find("PushButton").GetComponent<Button>();
 	}
+	private Button GetUAButton()
+	{
+		MyLog.D("GetUAButton");
+		return GameObject.Find("UAButton").GetComponent<Button>();
+	}
+	private Button GetFilePathButton()
+	{
+		MyLog.D("GetFilePathButton");
+		return GameObject.Find("FilePathButton").GetComponent<Button>();
+	}
+	private Button GetCheckButton()
+	{
+		MyLog.D("GetCheckButton");
+		return GameObject.Find("CheckButton").GetComponent<Button>();
+	}
+
+	private Button GetDeleteButton()
+	{
+		MyLog.D("GetDeleteButton");
+		return GameObject.Find("DeleteButton").GetComponent<Button>();
+	}
+
+	private Button GetNativeReadButton()
+	{
+		MyLog.D("GetNativeReadButton");
+		return GameObject.Find("NativeReadButton").GetComponent<Button>();
+	}
+
+	private Button GetNativeWriteButton()
+	{
+		MyLog.D("GetNativeWriteButton");
+		return GameObject.Find("NativeWriteButton").GetComponent<Button>();
+	}
+
 	void OnGUI () {
 		// Plane plane = GetLogPlane();
 		MyLog.DrawLogWindow(new Rect(10, 10, 1600, 1000));
--- Assets/Plugins/Android/SgpJni.cs
+++ Assets/Plugins/Android/SgpJni.cs
@@ -4,80 +4,136 @@ using System.Collections;
 using System.Runtime.InteropServices;
 
 public class SgpJni {
-	
-	[DllImport ("SgpUnity")] private static extern float FooPluginFunction();
-	[DllImport ("SgpUnity")] private static extern int Hoge();
-	[DllImport ("SgpUnity")] private static extern void getCacheDir();
-	[DllImport ("SgpUnity")] private static extern IntPtr getCommonFileName (IntPtr filename, int svno);
-	[DllImport ("SgpUnity")] private static extern IntPtr getNotCommonFileName (IntPtr filename, int svno);
-	[DllImport ("SgpUnity")] private static extern int nativeWriteCommonFile (IntPtr filename, IntPtr text, int svno);
-	[DllImport ("SgpUnity")] private static extern IntPtr nativeReadCommonFile (IntPtr filename, int svno);
-	[DllImport ("SgpUnity", EntryPoint="getMyName")] private static extern IntPtr CallNativeAndroid ();
-	//[DllImport ("SgpUnity", EntryPoint="getMyName")] private static extern void CallNativeAndroid ();
 
-//	public const int Release = 0;
-//	public const int Staging = 1;
-//	public const int Debug   = 2;
+	[DllImport ("SgpUnity")] private static extern IntPtr nativeReadFile  (IntPtr filename, int common, int svno);
+	[DllImport ("SgpUnity")] private static extern int    nativeWriteFile (IntPtr filename, IntPtr text, int common, int svno);
+	[DllImport ("SgpUnity")] private static extern IntPtr nativeGetUserAgent();
+	[DllImport ("SgpUnity")] private static extern int    nativeCheckNT();
+	[DllImport ("SgpUnity")] private static extern int    nativeDeleteFile (IntPtr filename, int common, int svno);
+	[DllImport ("SgpUnity")] private static extern IntPtr nativeTest();
 
-	public static float floatFunction() {
-		return FooPluginFunction();
-	}
-	public static int HogeTest() {
-		// MyLog.I("HogeTest");
-		return Hoge();
-	}
+	[DllImport ("SgpUnity")] private static extern IntPtr nativeSolReadFile (IntPtr filename, int common, int svno);
+	[DllImport ("SgpUnity")] private static extern IntPtr getFileNameToUnity (IntPtr filename, int common, int svno);
 
-	public static string toGetMyName() {
-		IntPtr ptr1 = Marshal.StringToHGlobalAnsi(null);
-		IntPtr ptr2 = CallNativeAndroid();
-		string result = Marshal.PtrToStringAnsi(ptr2);
+	// [DllImport ("SgpUnity", EntryPoint="getMyName")] private static extern IntPtr CallNativeAndroid();
+	[DllImport ("SgpUnity")] private static extern IntPtr getMyName();
 
-		Marshal.FreeHGlobal(ptr2);
-		Debug.Log("toGetMyName = " + result);
-		return result;
-	}
+	// static string  new_name = ".testJniUnity";
 
-	public static string toGetCommonFileName(string filename, int svno) {
+	// char* nativeReadFile(char* filename, int common, int svno)
+	public static string CallNativeReadFile(string filename, int common, int svno) {
+		Debug.Log("CallNativeReadFile filename = " + filename + " common = " + common + " svno = " + 0);
 		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
-		IntPtr pResult = getCommonFileName(pFilename, svno);
+		IntPtr pResult = nativeReadFile(pFilename, common, svno);
 		string result = Marshal.PtrToStringAnsi(pResult);
 
 		Marshal.FreeHGlobal(pFilename);
-		Debug.Log("toGetCommonFilename = " + result);
+		Debug.Log("CallNativeReadFile = " + result);
 		return result;
 	}
 
-	public static string toGetNotCommonFileName(string filename, int svno) {
+	// 	int nativeWriteFile(const char* filename, char* text, int common, int svno);
+	public static int CallnativeWriteFile(string filename, string text, int common, int svno) {
+		Debug.Log("CallnativeWriteFile filename = " + filename);
+
+		IntPtr pFilename = IntPtr.Zero;
+		IntPtr pText = IntPtr.Zero;
+		int result = 0;
+		try	{
+			System.Runtime.CompilerServices.RuntimeHelpers.PrepareConstrainedRegions();
+			try { }
+			finally	{
+				pFilename = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(filename);
+				pText = System.Runtime.InteropServices.Marshal.StringToHGlobalAnsi(text);
+			}
+			result = nativeWriteFile(pFilename, pText, common, svno);
+			// ここに処理を記載する
+		} finally {
+			if (pFilename != IntPtr.Zero) {
+				System.Runtime.InteropServices.Marshal.FreeHGlobal(pFilename);
+			}
+			if(pText != IntPtr.Zero) {
+				System.Runtime.InteropServices.Marshal.FreeHGlobal(pText);
+			}
+		}
+
+//		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
+//		IntPtr pText = Marshal.StringToHGlobalAnsi(text);
+//		int result = nativeWriteFile(pFilename, pText, common, svno);
+//
+//		Marshal.FreeHGlobal(pFilename);
+//		Marshal.FreeHGlobal(pText);
+
+		Debug.Log("CallnativeWriteFile = " + result);
+		return result;
+	}
+
+	// char* nativeGetUserAgent();
+	public static string CallnativeGetUserAgent() {
+		IntPtr pResult = nativeGetUserAgent();
+		string result = Marshal.PtrToStringAnsi(pResult);
+		Marshal.FreeHGlobal(pResult);
+		Debug.Log("CallnativeGetUserAgent = " + result);
+		return result;
+	}
+
+	// int nativeCheckNT();
+	public static int CallnativeCheckNT() {
+		int result = nativeCheckNT();
+		Debug.Log("CallnativeCheckNT = " + result);
+		return result;
+	}
+
+	// int nativeDeleteFile(const char* filename, int common, int svno);
+	public static int CallNativeDeleteFile(string filename, int common, int svno) {
+		Debug.Log("CallnativeWriteFile filename = " + filename);
 		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
-		IntPtr pResult = getNotCommonFileName(pFilename, svno);
+		int result = nativeDeleteFile(pFilename, common, svno);
+		Marshal.FreeHGlobal(pFilename);
+
+		Debug.Log("CallNativeDeleteFile = " + result);
+		return result;
+	}
+
+	// char* nativeTest()
+	public static string CallnativeTest() {
+		IntPtr pResult = nativeTest();
+		string result = Marshal.PtrToStringAnsi(pResult);
+		Marshal.FreeHGlobal(pResult);
+		Debug.Log("CallnativeTest = " + result);
+		return result;
+	}
+
+	//	char* nativeSolReadFile(const char* filename, int common, int svno);
+	public static string CallnativeSolReadFile(string filename, int common, int svno) {
+		Debug.Log("CallnativeSolReadFile filename = " + filename + " common = " + common + " svno = " + 0);
+		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
+		IntPtr pResult = nativeSolReadFile(pFilename, common, svno);
 		string result = Marshal.PtrToStringAnsi(pResult);
 
 		Marshal.FreeHGlobal(pFilename);
-		Debug.Log("toGetNonCommonFilename = " + result);
+		// Debug.Log("CallnativeSolReadFile = " + result);
 		return result;
 	}
 
-	// int nativeWriteFile(IntPtr filename, IntPtr text);
-	public static int toNativeWriteCommonFile(string filename, string text, int svno) {
-		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
-		IntPtr pText = Marshal.StringToHGlobalAnsi(text);
-		int result = nativeWriteCommonFile(pFilename, pText, svno);
-
-		Marshal.FreeHGlobal(pFilename);
-		Marshal.FreeHGlobal(pText);
-
-		Debug.Log("toNativeWriteCommonFile = " + result);
-		return result;
-	}
-
-	// char* nativeReadCommonFile(IntPtr filename);
-	public static string toNativeReadCommonFile(string filename, int svno) {
-		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
-		IntPtr pResult = nativeReadCommonFile(pFilename, svno);
+	public static string CallGetMyName() {
+		IntPtr pResult = getMyName();
 		string result = Marshal.PtrToStringAnsi(pResult);
-		Marshal.FreeHGlobal(pFilename);
 
-		// Debug.Log("toNativeReadCommonFile = " + result);
+		Marshal.FreeHGlobal(pResult);
+		Debug.Log("CallGetMyName = " + result);
+		return result;
+	}
+
+	// 	char* getFileNameToUnity(const char* filename, int common, int svno)
+	public static string CallgGetFileNameToUnity(string filename, int common, int svno) {
+		Debug.Log("CallgGetFileNameToUnity filename = " + filename);
+		IntPtr pFilename = Marshal.StringToHGlobalAnsi(filename);
+		IntPtr pResult = getFileNameToUnity(pFilename, common, svno);
+		string result = Marshal.PtrToStringAnsi(pResult);
+
+		Marshal.FreeHGlobal(pFilename);
+		Debug.Log("CallgGetFileNameToUnity = " + result);
 		return result;
 	}
 }
